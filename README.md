8084cb41997c29d2b8d1623b502bb16c12959c3c
halfblue74 <halfblue74@hotmail.com>
2020-04-24 14:57:33 +0900

    커밋_20200424_2

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # WeTubeCom
 
 Cloning Youtube with Vanilla and NodeJS
+
+## Pages:
+
+  - [ ] Main
+  - [x] Join
+  - [x] Login
+  - [x] Search
+  - [ ] User Detail
+  - [ ] Edit Profile
+  - [ ] Change Password
+  - [ ] Upload
+  - [ ] Video Detail
+  - [ ] Edit Video 
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -11,10 +11,10 @@ const userRouter = express.Router();
 
 //routing 이란? 어플리케이션 서버에서 경로를 제어하는 것을 목적. 목적지 까지 갈 수 있는 여러 경로 중 한 가지 경로를 설정해 주는 과정
 
-userRouter.get(routes.users, users);
-userRouter.get(routes.userDetail, userDetail);
+//userRouter.get(routes.users, users);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.userDetail, userDetail);
 
 
 export default userRouter;
